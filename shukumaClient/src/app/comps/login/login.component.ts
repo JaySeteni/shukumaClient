626a134a285cd1b3b97429ab582d4318d7a9e2b2
JaySeteni <jay.seteni@gmail.com>
2024-06-09 22:41:52 +0200

    registration and login complete

--- shukumaClient/src/app/comps/login/login.component.ts
+++ shukumaClient/src/app/comps/login/login.component.ts
@@ -1,20 +1,75 @@
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+// // import { ALL } from 'dns';
+
+
+// @Component({
+//   selector: 'app-login',
+//   templateUrl: './login.component.html',
+//   styleUrl: './login.component.css'
+// })
+// export class LoginComponent {
+
+//   signUpUsers: any[] = [];
+//   loginObj: any = { 
+//     email: '',
+//     password: '',
+//   }
+  
+//   loginForm: FormGroup;
+  
+//   constructor(private router: Router, private fb: FormBuilder) {
+//     this.loginForm = this.fb.group({
+//       email: ['', [Validators.required, Validators.email]],
+//       password: ['', Validators.required]
+//   }); 
+//   }
+
+//   ngOnInit(): void {
+//     const localData = localStorage.getItem('signUpUsers');
+//     if(localData != null) {
+//       this.signUpUsers = JSON.parse(localData)
+
+//     }
+//     // console.log(localData)
+//   }
+//   OnLogin() {
+
+//     const isUserExist = this.signUpUsers.find(
+//       m => m.email == this.loginForm.value.email 
+//       && m.password == this.loginForm.value.password );
+//     console.log(isUserExist)
+//     if (isUserExist !== undefined) {
+//       alert('Logged in successfully');
+//       console.log(this.signUpUsers);
+//       this.loginForm.reset();
+//       this.router.navigate(['/home']); // navigate to the home screen
+//     } else {
+//       alert('Incorrect credentials');
+//     }
+//   }
+
+// }
+
+
+// --- 
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
-
-// import { ALL } from 'dns';
-
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
   signUpUsers: any[] = [];
-  loginObj: any = { 
+  loginObj: { [key: string]: string } = { 
     email: '',
     password: '',
   }
@@ -25,31 +80,27 @@ export class LoginComponent {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
-  }); 
+    }); 
   }
 
   ngOnInit(): void {
     const localData = localStorage.getItem('signUpUsers');
-    if(localData != null) {
-      this.signUpUsers = JSON.parse(localData)
-
+    if (localData != null) {
+      this.signUpUsers = JSON.parse(localData);
     }
-    // console.log(localData)
   }
-  OnLogin() {
 
+  OnLogin(): void {
     const isUserExist = this.signUpUsers.find(
-      m => m.email == this.loginForm.value.email 
-      && m.password == this.loginForm.value.password );
-    console.log(isUserExist)
+      user => user['email'] === this.loginForm.value.email && user['password'] === this.loginForm.value.password
+    );
+
     if (isUserExist !== undefined) {
       alert('Logged in successfully');
-      console.log(this.signUpUsers);
       this.loginForm.reset();
       this.router.navigate(['/home']); // navigate to the home screen
     } else {
       alert('Incorrect credentials');
     }
   }
-
 }
