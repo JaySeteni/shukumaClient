e9cf4c9dd30a49b55dd234300d1dee84011438e8
D3konR3kon <dilavuyo@outlook.com>
2024-06-18 22:51:05 +0200

    WHY IS Footer nav placed in app.html?

--- shukumaClient/src/app/app.module.ts
+++ shukumaClient/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent} from './comps/register/register.component'
-import { LoginComponent } from './comps/login/login.component';
 import { NavComponent } from './comps/nav/nav.component';
 import { AdminComponent } from './comps/admin/admin.component';
 import { HomeComponent } from './comps/home/home.component';
@@ -16,6 +15,8 @@ import { FooterComponent } from './comps/footer/footer.component';
 import { UserprofileComponent } from './comps/userprofile/userprofile.component';
 import { CategoriesComponent } from './comps/categories/categories.component';
 import { SingleproductComponent } from './comps/singleproduct/singleproduct.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './comps/login/login.component';
 
 
 @NgModule({
