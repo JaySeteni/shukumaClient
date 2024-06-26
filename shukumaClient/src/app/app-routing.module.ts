565ae05b6f0a330fabaee50972c7c56122ee9186
Tsepo <3tnyarashe@gmail.com>
2024-06-26 16:24:34 +0200

    update

--- shukumaClient/src/app/app-routing.module.ts
+++ shukumaClient/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { UserprofileComponent } from './comps/userprofile/userprofile.component'
 import { CartComponent } from './comps/cart/cart.component';
 import { OrderComponent } from './comps/order/order.component';
 import { FooterComponent } from './comps/footer/footer.component';
+import { TrackDeliveriesComponent } from './track-deliveries/track-deliveries.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'welcome' , pathMatch: 'full'},
@@ -29,7 +30,8 @@ const routes: Routes = [
   {path: 'cart', component: CartComponent},
   {path: 'view_item/', component: SingleproductComponent},
   {path: 'order',component: OrderComponent},
-  {path: 'footer', component: FooterComponent}
+  {path: 'footer', component: FooterComponent},
+  {path: 'track-deliveries', component: TrackDeliveriesComponent}
 
 ];
 @NgModule({
