033d4a3483c9864c5db78f2d5b91ef5b54546d29
Tsepo <3tnyarashe@gmail.com>
2024-06-27 14:49:12 +0200

    user profile update

--- shukumaClient/src/app/comps/userprofile/userprofile.component.ts
+++ shukumaClient/src/app/comps/userprofile/userprofile.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+interface UserProfile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phoneNumber: string;
+  avatarUrl: string; 
+  bio: string; 
+  location: string; 
+  website: string; 
+  socialLinks: {
+    twitter?: string;
+    instagram?: string;
+    linkedin?: string;
+  }
+}
+
 
 @Component({
   selector: 'app-userprofile',
   templateUrl: './userprofile.component.html',
   styleUrl: './userprofile.component.css'
 })
-export class UserprofileComponent {
 
+export class UserprofileComponent implements OnInit {
+
+    mockUserProfile: any = {
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    phoneNumber: '+1234567890',
+    avatarUrl: 'https://placeimg.com/150/150/people', // Placeholder image
+    bio: 'Software Engineer passionate about building web applications.',
+    location: 'New York, USA',
+    website: 'https://johndoe.com', // Optional website
+    socialLinks: {
+      twitter: 'johndoe',
+      instagram: 'johndoe_dev',
+    }
+  }
+  
+  ngOnInit(): void {
+    
+  }
 }
