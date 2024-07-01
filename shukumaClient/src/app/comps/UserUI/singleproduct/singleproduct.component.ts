3758e39eaec05cb26ba6c445c5910a4622cec04a
buhleqampi <buhle3857@gmail.com>
2024-07-01 17:57:19 +0200

    merging done

--- shukumaClient/src/app/comps/UserUI/singleproduct/singleproduct.component.ts
+++ shukumaClient/src/app/comps/UserUI/singleproduct/singleproduct.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MainService } from '../../../main.service';
+// import { MainService } from '../../../main.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../interface/product'; // Assuming Product interface
 import { CartService } from '../../../services/cart.service';
@@ -15,48 +15,48 @@ export class SingleproductComponent implements OnInit {
   selectedProduct: any
 
   constructor(
-    private route: ActivatedRoute,
-    private mainService: MainService,
-    private cartService: CartService,
+    private route: ActivatedRoute
+    // private mainService: MainService,
+    // private cartService: CartService,
   ) { }
 
   ngOnInit(): void {
     this.getAllProducts()
-    this.getOneProduct()
+  
   }
 
   getAllProducts(){
 
-    this.mainService.getAllProducts().subscribe({
-      next: data =>{
-      this.products = data.products
-      },
-      error: err=>{
-        console.log(err)
-      }
-    })
-  }
-  getOneProduct(): void {
-    const id = this.route.snapshot.paramMap.get('id');
+  //   this.mainService.getAllProducts().subscribe({
+  //     next: data =>{
+  //     this.products = data.products
+  //     },
+  //     error: err=>{
+  //       console.log(err)
+  //     }
+  //   })
+  // }
+  // getOneProduct(): void {
+  //   const id = this.route.snapshot.paramMap.get('id');
 
 
-      this.mainService.getProduct(id).subscribe({
-        next: (res: any) => {
-          this.selectedProduct = res.product;
-        },
-        error: (err: any) => {
-          console.error("An error occurred while fetching product:", err);
-          // Handle errors gracefully (e.g., display an error message)
-        }
-      });
-  }
+  //     this.mainService.getProduct(id).subscribe({
+  //       next: (res: any) => {
+  //         this.selectedProduct = res.product;
+  //       },
+  //       error: (err: any) => {
+  //         console.error("An error occurred while fetching product:", err);
+  //         // Handle errors gracefully (e.g., display an error message)
+  //       }
+  //     });
+  // }
 
-  addToCart(item: Product): void {
-    this.cartService.addToCart(item); // Assuming cartService exists in your setup
-    this.isAdded = this.cartService.getAdded(); // Assuming getAdded returns a boolean
-  }
+  // addToCart(item: Product): void {
+  //   this.cartService.addToCart(item); // Assuming cartService exists in your setup
+  //   this.isAdded = this.cartService.getAdded(); // Assuming getAdded returns a boolean
+  // }
 
-  saveForLaterButton(product: Product): void {
-    this.cartService.addToWishlist(product); // Assuming cartService exists
+  // saveForLaterButton(product: Product): void {
+  //   this.cartService.addToWishlist(product); // Assuming cartService exists
   }
 }
