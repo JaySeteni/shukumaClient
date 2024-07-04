b61ba52a42e4d5feca47837f3d35f42bc225c7e3
add coments <lukhanyo95@gmail.com>
2024-07-04 14:22:46 +0200

    populate the product service with methods

---
+++ src/app/services/product-service/product.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  private baseUrl: string = "http://localhost:3000/v1/product/";
+
+  constructor (private _http : HttpClient) {
+
+  }
+  
+  getAllProducts(): Observable<any[]>{
+    return this._http.get<any[]>(`${this.baseUrl}articles`)
+  }
+
+  getProduct(id: any): Observable<any> {
+    return this._http.get<any>(`${this.baseUrl}/article/${id}`)
+  }
+
+
+}
