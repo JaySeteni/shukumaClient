d5d57605dc601735c12f7032595b78d61db99bd4
DiniJNS <jonas.dinilethu@gmail.com>
2024-07-04 15:39:21 +0200

    register-business

--- src/app/comps/BusinessUI/register-business/register-business.component.ts
+++ src/app/comps/BusinessUI/register-business/register-business.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-business',
@@ -6,8 +8,42 @@ import { Component } from '@angular/core';
   styleUrl: './register-business.component.css'
 })
 export class RegisterBusinessComponent {
-  registerBusiness() {
-    // Logic for registering the business
-    // You can access the form data using Angular's ngModel or form controls
+  showLoginPage: boolean = true;
+  registerBusinessPage: boolean = false;
+  
+  signUpUsers: any[] = [];
+  loginObj: { [key: string]: string } = { 
+    email: '',
+    password: '',
+  }
+  
+  loginForm: FormGroup;
+  
+  constructor(private router: Router, private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    }); 
+  }
+
+  ngOnInit(): void {
+    const localData = localStorage.getItem('signUpUsers');
+    if (localData != null) {
+      this.signUpUsers = JSON.parse(localData);
+    }
+  }
+
+  OnLogin(): void {
+    const isUserExist = this.signUpUsers.find(
+      user => user['email'] === this.loginForm.value.email && user['password'] === this.loginForm.value.password
+    );
+
+    if (isUserExist !== undefined) {
+      alert('Logged in successfully');
+      this.loginForm.reset();
+      this.router.navigate(['userhome']); // navigate to the home screen
+    } else {
+      alert('Incorrect credentials');
+    }
   }
 }
