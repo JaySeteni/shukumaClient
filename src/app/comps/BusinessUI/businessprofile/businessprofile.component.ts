e4a4a78f9d67cbd722ea6b3cac9bf8ce2594f5a5
JaySeteni <jay.seteni@gmail.com>
2024-07-05 16:12:20 +0200

    pushing businessProfile

--- src/app/comps/BusinessUI/businessprofile/businessprofile.component.ts
+++ src/app/comps/BusinessUI/businessprofile/businessprofile.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface BusinessProfile {
+  name: {
+    type: string,
+    required:true;
+    trim: true,
+    unique: true,
+  };
+  email:{
+    type: string,
+    required: true,
+    trim: true,
+  };
+  description: {
+    type: string,
+    required: true,
+    trim: true,
+  };
+  industry: string,
+  tel_no: {
+    type:string,
+    required: true,
+    trim: true,
+  };
+  avatarUrl: string;
+  address: string,
+  img: {
+    type: string,
+    default: "https://www.thesait.org.za/global_graphics/default-store-350x350.jpg"
+  };
+}
 
 @Component({
   selector: 'app-businessprofile',
   templateUrl: './businessprofile.component.html',
   styleUrl: './businessprofile.component.css'
 })
-export class BusinessprofileComponent {
+export class BusinessprofileComponent implements OnInit{
 
+  mockBusinessProfile: any = {
+    name: 'mzimkhulu',
+    email: 'john.doe@example.com',
+    description: 'Mzimkhulu is a gas selling company LTD. ',
+    tel_no: '+1234567890',
+    industry: 'gas',
+    avatarUrl: 'https://placeimg.com/150/150/people', 
+    address: '123 Joe Crescent, New York, USA, 7785',
+    img: 'https://www.thesait.org.za/global_graphics/default-store-350x350.jpg'
+  }
+  
+  ngOnInit(): void {
+    
+  }
 }
