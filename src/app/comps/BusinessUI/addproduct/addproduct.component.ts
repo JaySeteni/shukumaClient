ec4821b5cbdd1a2feb0f4df75bd715cc62a26eb4
buhleqampi <buhle3857@gmail.com>
2024-07-07 17:50:12 +0200

    changes from Tsepo

--- src/app/comps/BusinessUI/addproduct/addproduct.component.ts
+++ src/app/comps/BusinessUI/addproduct/addproduct.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
-import { Product } from '../../../interfaces/product';
+import { ProductService } from '../../../services/product-service/product.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-addproduct',
   templateUrl: './addproduct.component.html',
-  styleUrl: './addproduct.component.css'
+  styleUrls: ['./addproduct.component.css']
 })
-export class AddproductComponent {
+export class AddProductComponent {
+  productForm: FormGroup;
+  image: File | null = null;
+  message: string = '';
 
-  addProduct() {
- 
+  constructor(
+    private productService: ProductService,
+    private fb: FormBuilder
+  ) {
+    this.productForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(0)]],
+      images: ['', Validators.required]
+    });
   }
+
+  // onFileChange(event: any) {
+  //   if (event.target.files.length > 0) {
+  //     this.image = event.target.files[0];
+  //   }
+  // }
+
+  // submitForm() {
+  //   if (this.productForm.invalid || !this.image) {
+  //     this.message = 'Please fill all the required fields and upload an image.';
+  //     return;
+  //   }
+
+  //   const formData = new FormData();
+  //   formData.append('name', this.productForm.get('name')?.value);
+  //   formData.append('description', this.productForm.get('description')?.value);
+  //   formData.append('price', this.productForm.get('price')?.value);
+  //   formData.append('images', this.image);
+
+  //   this.productService.createProduct(formData).subscribe(
+  //     response => {
+  //       this.message = 'Product created successfully!';
+  //       this.productForm.reset();
+  //       this.image = null;
+  //     },
+  //     error => {
+  //       this.message = 'Error creating product';
+  //       console.error(error);
+  //     }
+  //   );
+  // }
 }
+
+
