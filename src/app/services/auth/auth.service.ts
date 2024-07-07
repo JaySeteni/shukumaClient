b6170aa674f3f20189ba9f6bc6379631e4e96951
Tsepo <3tnyarashe@gmail.com>
2024-07-07 17:44:46 +0200

    update

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
 import { UserRegister } from '../../interfaces/userRegister';
+import { UserLogin } from '../../interfaces/userLogin';
+import { Observable } from 'rxjs';
+import { SignInData } from '../../interfaces/signInData';
 
 @Injectable({
   providedIn: 'root'
@@ -16,5 +19,13 @@ export class AuthService {
   
   registerUser(userData: UserRegister) {
     return this.http.post('http://localhost:3000/v1/auth/register', userData); // Replace with your API endpoint
+  }
+
+  login(userData: UserLogin) {
+    this.http.post<SignInData>('http://localhost:3000/v1/auth/signin', userData).subscribe({
+      next: (res: SignInData) => {
+        console.log(res)
+      }
+    })
   }
 }
