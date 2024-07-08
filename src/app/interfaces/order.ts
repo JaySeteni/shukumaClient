e9277cd510a54b5c95b882be5097a27ad0f431b4
JaySeteni <jay.seteni@gmail.com>
2024-07-08 13:36:13 +0200

    updates  on order

--- src/app/interfaces/order.ts
+++ src/app/interfaces/order.ts
@@ -2,22 +2,27 @@ export interface Order {
 Orders: Order[];
 cartId: string;
 createdAt: any;
-  id: unknown;
-  description: string;
-  total: number;
-  user: string;
-  address: {
-    street: string;
-    city: string;
-    postalCode: string;
-    country: string;
-  };
-  items: {
-    productId: unknown;
-    quantity: number;
-    _id: any;
-    length: number;
-  };
-  orderNumber: number;
-  status: string;
+id: unknown;
+description: string;
+total: number;
+userId: string;
+address: {
+  street: string;
+  city: string;
+  postalCode: string;
+  country: string;
+};
+items: {
+  productId: unknown;
+  quantity: number;
+  _id: any;
+  length: number;
+};
+orderNumber: number;
+status: string;
+driverId: string;
+location:{
+  lat:any; 
+  long:any;
+} 
 }
