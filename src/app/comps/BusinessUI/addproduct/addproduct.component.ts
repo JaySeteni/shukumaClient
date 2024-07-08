c6229680f850e7bca5da7cdcb8b9e101c35783da
buhleqampi <buhle3857@gmail.com>
2024-07-08 13:34:51 +0200

    demo day near

--- src/app/comps/BusinessUI/addproduct/addproduct.component.ts
+++ src/app/comps/BusinessUI/addproduct/addproduct.component.ts
@@ -24,36 +24,38 @@ export class AddProductComponent {
     });
   }
 
-  // onFileChange(event: any) {
-  //   if (event.target.files.length > 0) {
-  //     this.image = event.target.files[0];
-  //   }
-  // }
+  onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      this.image = event.target.files[0];
+    }
+  }
 
-  // submitForm() {
-  //   if (this.productForm.invalid || !this.image) {
-  //     this.message = 'Please fill all the required fields and upload an image.';
-  //     return;
-  //   }
+  submitForm() {
+    if (this.productForm.invalid || !this.image) {
+      this.message = 'Please fill all the required fields and upload an image.';
+      return;
+    }
 
-  //   const formData = new FormData();
-  //   formData.append('name', this.productForm.get('name')?.value);
-  //   formData.append('description', this.productForm.get('description')?.value);
-  //   formData.append('price', this.productForm.get('price')?.value);
-  //   formData.append('images', this.image);
+    const formData = new FormData();
+    formData.append('name', this.productForm.get('name')?.value);
+    formData.append('description', this.productForm.get('description')?.value);
+    formData.append('price', this.productForm.get('price')?.value);
+    formData.append('images', this.image);
 
-  //   this.productService.createProduct(formData).subscribe(
-  //     response => {
-  //       this.message = 'Product created successfully!';
-  //       this.productForm.reset();
-  //       this.image = null;
-  //     },
-  //     error => {
-  //       this.message = 'Error creating product';
-  //       console.error(error);
-  //     }
-  //   );
-  // }
+    this.productService.createProduct(formData).subscribe(
+      response => {
+        this.message = 'Product created successfully!';
+        this.productForm.reset();
+        this.image = null;
+
+        console.log(this.productForm)
+      },
+      error => {
+        this.message = 'Error creating product';
+        console.error(error);
+      }
+    );
+  }
 }
 
 
