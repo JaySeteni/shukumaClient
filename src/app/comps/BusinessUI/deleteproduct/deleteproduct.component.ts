41a42cf13c52281bceab69ecb9acc14978996037
buhleqampi <buhle3857@gmail.com>
2024-07-15 18:05:52 +0200

    update/delete

---
+++ src/app/comps/BusinessUI/deleteproduct/deleteproduct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-deleteproduct',
+  templateUrl: './deleteproduct.component.html',
+  styleUrl: './deleteproduct.component.css'
+})
+export class DeleteproductComponent {
+
+}
---
+++ src/app/comps/BusinessUI/updateproduct/updateproduct.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-updateproduct',
+  templateUrl: './updateproduct.component.html',
+  styleUrl: './updateproduct.component.css'
+})
+export class UpdateproductComponent {
+
+}
